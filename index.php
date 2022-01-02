4e24f2cfea5c604ec7d5e87a3ae39796a78de71f
Alexander <formysite.ru@yandex.ru>
2022-01-02 14:01:51 +0300

    start_lesson

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    $nickname=" Alexander";
+    $hello="Привет";
+    $text_hello="Продолжаем обучение.";
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Функции для работы с массивом</title>
+</head>
+<body>
+    <header class="">
+        <?php echo $hello . $nickname ."<br>" . $text_hello; ?>
+        <h2>Функции для работы с массивами.</h2>
+    </header>
+    
+</body>
+</html>
